b3a246025e586e5451f814556dd8bcb433b45b04
Amanda Park <wd1204@uw.edu>
2019-08-07 14:28:40 -0700

    link added

--- scripts.js
+++ scripts.js
@@ -1,37 +1,73 @@
 const ICNDB_URl = "http://api.icndb.com/jokes/random"; 
-const DONALD_TRUMP = "";
-
+const DONALD_TRUMP = "https://icanhazdadjoke.com/";
+const 
 //only fetching one joke 
-fetch(ICNDB_URl)
+
+function fetchOne() {
+    fetch(ICNDB_URl)
     .then(checkStatus)
     .then(resp => resp.json())
     .then((response) => {
-        appendToPage(response, "one");
+        icndbAppendToPage(response, "one");
     })
     .catch(console.error);
+}
 
+let fetchData = {
+    method: 'GET',
+    Headers: {
+        'Content-Type': 'application/json',
+        "Accept" : 'application/json',
+        cache: 'no-cache',
+        mode: 'cors'
+        }
+}
+
+function fetchTwo() {
+    fetch(DONALD_TRUMP,  {
+        method: 'GET',
+        headers: {
+            "Accept" : 'application/json',
+        }
+    })
+    .then(checkStatus)
+    .then(resp => {
+        console.log(resp);
+        return resp.json();
+    })
+    .then((response) => {
+        console.log(response);
+        trumpAppendToPage(response, "two");
+    })
+    .catch(console.error);
+}
+ 
+
+function checkStatus(response) {
+    console.log(response);
+    if (!response.ok) {
+        throw Error("Error in request: " + response.statusText);
+    }
+    //reponse object
+    return response;
+}
+
+function icndbAppendToPage(response, name) {
+    console.log("inside");
+    let homeJokePost = response.value.joke;
+    // $("#id").append(homeJokePost);
+    document.getElementById(name).append(homeJokePost);
+}
 
 function init() {
     fetchOne();
     fetchTwo();
-    fetchThree();
-    fetchFour();
-}
-function fetchOne() {
-
-}
-function checkStatus(resposnse) {
-    if (!resposnse.ok) {
-        throw Error("Error in request: " + response.statusText);
-    }
-    //reponse object
-    return resposnse;
+    // fetchThree();
+    // fetchFour();
 }
 
-function appendToPage(response, name) {
-    console.log("inside");
-    let homeJokePost = response.value.joke;
-    console.log(homeJokePost);
-    console.log(document.getElementById("one"));
+function trumpAppendToPage(response, name) {
+    let homeJokePost = response.joke;
     document.getElementById(name).append(homeJokePost);
 }
+init();
